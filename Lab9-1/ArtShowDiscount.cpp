8193284b846ae4d76423cbde4d46622424190dd3
Drouge7414 <108477680+Drouge7414@users.noreply.github.com>
2024-07-11 10:37:42 -0400

    Create ArtShowDiscount.cpp

---
+++ Lab9-1/ArtShowDiscount.cpp
@@ -0,0 +1,37 @@
+// ArtShowDiscount.cpp - This program determines if an art show attendee gets a 5% discount for preregistering. 
+// Input:  Interactive
+// Output:  A statement telling the user if they get a discount or no discount.  
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void discount();
+void noDiscount();
+
+int main() 
+{
+   string registerString;              			
+						
+   cout << "Did you preregister? Enter Y or N: ";
+   cin >> registerString; 
+		
+   // Test here. If = Y, call discount(), else call noDiscount(). 
+   if (registerString == "y" || registerString == "Y") {
+      discount();
+   }else {
+      noDiscount();
+   }
+   return 0;
+} // End of main function
+
+// Write discount function here
+	
+   void discount () {
+      cout << "You are pre-registered and qualify for a 5 percent discount." << endl;
+   }
+
+// Write noDiscount function here
+void noDiscount() {
+    cout << "Sorry, you did not pre-register and do not qualify for a 5 percent discount." << endl;
+}
