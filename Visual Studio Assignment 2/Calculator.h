9248e7585cba1b1cac0cfdc674b34eea2e6f4521
Drouge7414 <108477680+Drouge7414@users.noreply.github.com>
2024-07-17 14:13:37 -0400

    Create Calculator.h

---
+++ Visual Studio Assignment 2/Calculator.h
@@ -0,0 +1,8 @@
+//.Dylan Barrett
+#pragma once
+class Calculator // Tells the program that this is the class Calculator
+{
+public: // States this is a public function that can modified outside of this class
+	double Calculate(double x, char oper, double y); // Declares the function Calculate which will be used to preform mathmatical operations
+};
+
