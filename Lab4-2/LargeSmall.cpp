f46afc1172ee56a89eeb7f8016df000674808923
Drouge7414 <108477680+Drouge7414@users.noreply.github.com>
2024-07-28 18:28:01 -0400

    Create LargeSmall.cpp

---
+++ Lab4-2/LargeSmall.cpp
@@ -0,0 +1,36 @@
+// LargeSmall.cpp - This program calculates the largest and smallest of three integer values. 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   // Declare and initialize variables here
+   int largest;	// Largest of the three values
+   int smallest; // Smallest of the three values
+   int firstNumber = -50;	
+   int secondNumber = 53;
+   int thirdNumber = 78;
+
+
+   
+   // Write assignment, if, or if else statements here as appropriate
+	if (firstNumber >= secondNumber && firstNumber >=thirdNumber) 
+         largest = firstNumber;
+      else if (secondNumber >= firstNumber && secondNumber >= thirdNumber)
+         largest = secondNumber;
+      else
+         largest = thirdNumber;   
+
+    if (firstNumber <= secondNumber && firstNumber <=thirdNumber) 
+         smallest = firstNumber;
+      else if (secondNumber <= firstNumber && secondNumber <= thirdNumber)
+         smallest = secondNumber;
+      else
+         smallest = thirdNumber;   
+
+   // Output largest and smallest number. 
+   cout << "The largest value is " << largest << endl;
+   cout << "The smallest value is " << smallest << endl;
+   
+   return 0; 
+} 	
